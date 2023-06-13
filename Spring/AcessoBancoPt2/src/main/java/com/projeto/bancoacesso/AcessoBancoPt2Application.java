0d191a37e0aa1b91747bce758f4a07ec2a4858b2
Fernando Magaton <104094324+fernandoagmagaton@users.noreply.github.com>
2023-06-13 17:05:21 -0300

    Add files via upload

---
+++ Spring/AcessoBancoPt2/src/main/java/com/projeto/bancoacesso/AcessoBancoPt2Application.java
@@ -0,0 +1,13 @@
+package com.projeto.bancoacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoBancoPt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoBancoPt2Application.class, args);
+	}
+
+}
---
+++ Spring/AcessoBancoPt2/src/test/java/com/projeto/bancoacesso/AcessoBancoPt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.bancoacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessoBancoPt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
